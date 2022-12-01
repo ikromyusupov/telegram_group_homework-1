8cb52fdaa1fd153071818f33cddf45192885460a
ikromyusupov <@ikromyusupovdev@gmail.com>
2022-12-01 14:57:20 +0500

    last task done

--- find_user_message_count.py
+++ find_user_message_count.py
@@ -1,9 +1,6 @@
 from read_data import read_data
 from find_all_users_id import find_all_users_id
 
-data = read_data("data/result.json")
-users_id = find_all_users_id(data)
-
 def find_user_message_count(data: dict, users_id: str)->dict:
     """
     This function will find the user's message count.
@@ -14,4 +11,11 @@ def find_user_message_count(data: dict, users_id: str)->dict:
     Returns:
         dict: Number of messages of the users
     """
-    return len(users_id)
+    message_count = dict(zip(users_id, [0]*len(users_id)))
+
+    for message in data['messages']:
+        user_id = message.get('from_id', False)
+        if user_id and user_id in users_id:
+            message_count[user_id] += 1
+
+    return message_count
